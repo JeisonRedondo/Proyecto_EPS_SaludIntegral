8627aae5d4adb49e4a1df043c1b583589bf42b0c
AFiquitiva <jafiquitiva@gmail.com>
2021-10-05 19:41:24 -0500

    añadido medic manualmente

--- SaludIntegral.App.Consola/Program.cs
+++ SaludIntegral.App.Consola/Program.cs
@@ -7,11 +7,18 @@ namespace SaludIntegral.App.Consola
     class Program
     {
         private static IRepositoriesPatient _repoPatient = new RepositoriesPatient(new Persistencia.AppContext());
+        private static IRepository_Medic _repoMedic = new Repository_Medic(new Persistencia.AppContext());
         static void Main(string[] args)
         {
            Console.WriteLine("Hello World EF!");
            //AddPatient(); 
            SearchPatient(3);
+
+            //AddMedic();
+           //Find_Medic(1);
+           //Delete_Medic(3);
+           //Show_medics();
+
         }
 
         private static void AddPatient()
@@ -34,6 +41,51 @@ namespace SaludIntegral.App.Consola
             var patient1 = _repoPatient.GetPatient(idPatient);
             Console.WriteLine(patient1.Names+" "+patient1.Email);
         }
+
+
+       
+        //preguntar si la  fecha es mejor con data time
+        private static void AddMedic()
+        {
+            var medic = new Medic
+
+            {
+                
+                Names = "tania ospina",  
+                Age = "34",
+                Gender = "femenino",
+                Phone_number = "3209637410",
+                Email = "luisao@gmail.com",
+                Specialization = "cirujana"
+                //Locations = "Cartagena"
+                        
+            };
+            _repoMedic.AddMedic(medic);         
+
+        }
+
+        private static void Find_Medic(int Medic)
+        {
+            var medic = _repoMedic.GetMedic(Medic);
+            Console.WriteLine(medic.Names);
+        
+        }
+
+        private static void Delete_Medic(int Id_Medic)
+        {
+            _repoMedic.DeleteMedic(Id_Medic);
+            Console.WriteLine("Medic removed");
+        }
+
+       /* private static void Show_medics()
+        {
+            IEnumerable<Medic> Medicos = _repoMedic.GetAllMedics();
+            foreach (var medic in Medicos)
+            {
+                Console.WriteLine(medic.Names);
+            }
+        }*/
+
 
     }
 }

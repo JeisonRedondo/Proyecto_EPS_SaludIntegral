f63327774b70706c89f6a0c8e4c54e930c2d773b
Juan Camilo Ortiz <juancortizgonz@gmail.com>
2021-10-13 21:03:17 -0500

    Update frontend and add new layouts * Juan Ortiz

--- SaludIntegral.App.Frontend/wwwroot/js/site.js
+++ SaludIntegral.App.Frontend/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+var limit = 1;
+$('input.checkbox-account').on('change', function(evt) {
+   if($(this).siblings(':checked').length >= limit) {
+       this.checked = false;
+   }
+});
+
+$('input.checkbox-account').on('change', function(evt) {
+    const spanToChange = document.getElementById('typeAccount');
+    if(document.getElementById('doctor').checked) {
+        document.getElementById("continue").style.visibility = "visible";
+        spanToChange.innerHTML = "Doctor!";
+    } else if(document.getElementById('patient').checked) {
+        document.getElementById("continue").style.visibility = "visible";
+        spanToChange.innerHTML = "Patient!";
+    } else {
+        document.getElementById("continue").style.visibility = "hidden";
+    }
+ });

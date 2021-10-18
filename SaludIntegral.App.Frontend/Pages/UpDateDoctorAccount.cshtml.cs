f9e8af7f7f8d2b5ea12f7347b543f0cf22ffed3e
unknown <ingeniero.jonathantajan@gmail.com>
2021-10-17 22:36:48 -0500

    UpDateDoctorAccount 17102021

---
+++ SaludIntegral.App.Frontend/Pages/UpDateDoctorAccount.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SaludIntegral.App.Frontend.Pages
+{
+    public class UpDateDoctorAccountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
+/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace SaludIntegral.App.Frontend.Pages
+{
+    public class UpDateDoctorAccountModel : PageModel
+    {
+        private readonly ILogger<UpDateDoctorAccountModel> _logger;
+
+        public UpDateDoctorAccountModel(ILogger<UpDateDoctorAccountModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}*/
